f5ccf1d774103940f14cecd04107901ead6cb776
Jessie De La Cruz Santos <jessiedlcs@squareup.com>
2019-09-04 19:11:48 -0700

    Switched from RNCamerKit to RNCamera

--- reader-sdk-react-native-quickstart/app/screens/QRAuthorizationScreen.js
+++ reader-sdk-react-native-quickstart/app/screens/QRAuthorizationScreen.js
@@ -13,44 +13,95 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { CameraKitCameraScreen } from 'react-native-camera-kit';
+import {
+  StyleSheet, Text, TouchableOpacity, View,
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
 import { backgroundColor } from '../styles/common';
 
 export default class QRAuthorizationScreen extends Component {
-  state = {
-    receivedCode: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      receivedCode: false,
+    };
+
+    this.onSuccess = this.onSuccess.bind(this);
   }
 
-  async onSuccess(e) {
+  onSuccess(scanResult) {
     if (this.state.receivedCode) return;
     this.setState({ receivedCode: true });
-    this.props.navigation.navigate('Authorizing', { authCode: e.nativeEvent.codeStringValue });
+    this.props.navigation.navigate('Authorizing', { authCode: scanResult.data });
   }
 
   render() {
     const { goBack } = this.props.navigation;
+    console.log('hello');
     return (
-      <CameraKitCameraScreen
-        actions={{ leftButtonText: 'Cancel' }}
-        onBottomButtonPressed={() => goBack()}
-        showFrame
-        colorForScannerFrame={backgroundColor}
-        scanBarcode
-        onReadCode={e => this.onSuccess(e)}
-        hideControls={false}
-        heightForScannerFrame={300}
-        cameraOptions={{
-          flashMode: 'auto',
-          focusMode: 'on',
-          zoomMode: 'off',
-        }}
-      />
+      <View style={styles.container}>
+        <RNCamera
+          ref={(ref) => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.off}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onBarCodeRead={this.onSuccess}
+        />
+        <View style={styles.button}>
+          <TouchableOpacity onPress={() => goBack()} style={styles.capture}>
+            <Text style={styles.textSize}> Cancel </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  capture: {
+    alignSelf: 'center',
+    backgroundColor,
+    borderRadius: 5,
+    flex: 0,
+    margin: 20,
+    padding: 15,
+    paddingHorizontal: 20,
+  },
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
+    flexDirection: 'column',
+  },
+  preview: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  textSize: {
+    fontSize: 14,
+  },
+});
 
 QRAuthorizationScreen.propTypes = {
   navigation: PropTypes.object.isRequired,
