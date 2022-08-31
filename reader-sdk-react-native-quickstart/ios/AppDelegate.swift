7c29aca52d7c0362e906868e558d7c8e36e3e551
CharmisG <102352276+CharmisG@users.noreply.github.com>
2022-08-31 11:01:41 +0530

    Charmis/sdk upgradation (#188)
    * bottomView and deauthorize dialog
    
    * Sub folder swift conversion
    
    * Revert "support android 12+"
    
    * Update CheckoutScreen.tsx
    
    * plugin upgradation to 30
    
    * SDK Upgradation to 31
    
    * Change file name

---
+++ reader-sdk-react-native-quickstart/ios/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  RNReaderSDKSample
+
+import Foundation
+import SquareReaderSDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+   var bridge: RCTBridge!
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+     let jsCodeLocation: URL
+
+     jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
+     let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "RNReaderSDKSample", initialProperties: nil, launchOptions: launchOptions)
+     let rootViewController = UIViewController()
+     rootViewController.view = rootView
+
+     self.window = UIWindow(frame: UIScreen.main.bounds)
+     self.window?.rootViewController = rootViewController
+     self.window?.makeKeyAndVisible()
+     SQRDReaderSDK.initialize(applicationLaunchOptions: launchOptions)
+
+     return true
+   }
+}
