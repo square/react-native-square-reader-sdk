3e6c5d565543e4546700a3afa6c68f5d8c2b50cf
AJAYJ-09 <ajadhav@squareup.com>
2024-06-07 15:33:15 +0530

    Resolved Build Failure

--- reader-sdk-react-native-quickstart/babel.config.js
+++ reader-sdk-react-native-quickstart/babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  overrides: [{
+    "plugins": [
+      ["@babel/plugin-transform-private-methods", {
+      "loose": true
+    }]
+    ]
+  }]
 };
